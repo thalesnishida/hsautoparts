e39b96e28bf620a1785988701a001360ddb8cc5a
Thales Nishida <medeiros.tn@gmail.com>
2025-02-05 09:42:38 -0300

    Create PartCarTest

---
+++ src/test/kotlin/br/com/nishida/hsautocars/domain/value_objects/PartCarTest.kt
@@ -0,0 +1,10 @@
+package br.com.nishida.hsautocars.domain.value_objects
+
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.Test
+
+@SpringBootTest
+class PartCarTest {
+
+
+}
