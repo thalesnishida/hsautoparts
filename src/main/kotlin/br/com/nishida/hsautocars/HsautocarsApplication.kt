85c3a2d37c37fc0558fac243953ff3952e1291ef
Thales Nishida <medeiros.tn@gmail.com>
2025-02-05 09:39:54 -0300

    First commit

---
+++ src/main/kotlin/br/com/nishida/hsautocars/HsautocarsApplication.kt
@@ -0,0 +1,11 @@
+package br.com.nishida.hsautocars
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HsautocarsApplication
+
+fun main(args: Array<String>) {
+    runApplication<HsautocarsApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/nishida/hsautocars/HsautocarsApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.nishida.hsautocars
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HsautocarsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
